dadfe167ed26a0d66689b63dbb5fc6f0555f98bb
AyoubDammaq <ayoubdammak81@gmail.com>
2025-01-28 23:15:46 +0100

    Auto-generated commit

---
+++ browser/chunk-IUAIEAJH.js
@@ -0,0 +1,31 @@
+import{b as l,g as m,h as e,i as t,j as o,k as n,o as s}from"./chunk-LXRMDS5F.js";var d=class i{constructor(r){this.titleService=r;this.titleService.setTitle("Ayoub Dammak - About")}static \u0275fac=function(a){return new(a||i)(m(s))};static \u0275cmp=l({type:i,selectors:[["app-about"]],decls:18,vars:0,consts:[["id","about",1,"py-16","bg-white"],[1,"container","mx-auto"],[1,"text-4xl","font-bold","text-center","text-gray-800","mb-8","animate-fade-in"],[1,"grid","grid-cols-1","md:grid-cols-2","gap-12","items-center"],["aria-label","Profile Image",1,"flex","justify-center","animate-slide-in"],["src","https://avatars.githubusercontent.com/u/137606998?v=4","alt","Ayoub Dammak working on a project",1,"w-60","h-60","md:w-80","md:h-80","rounded-full","border-4","border-gray-300","shadow-lg","transform","hover:scale-105","hover:shadow-2xl","transition-transform","duration-300"],["aria-label","About Ayoub Dammak",1,"text-center","md:text-left","animate-fade-in"],[1,"text-3xl","font-semibold","text-gray-700","mb-4"],[1,"text-lg","text-gray-600","leading-relaxed","mb-4"],[1,"text-lg","text-gray-600","leading-relaxed","mb-6"],["href","https://drive.google.com/file/d/1-K4mxM4z_6LUT9mv6LxmEgDJZ2fQH7ne/view?usp=drive_open","target","_blank","download","Ayoub-Dammak-CV.pdf",1,"inline-block","bg-blue-600","text-white","px-6","py-2","rounded-md","shadow-md","hover:bg-blue-700","hover:shadow-lg","transform","hover:scale-105","transition-all","duration-300","group","relative"],[1,"uil","uil-file-download","ml-2"],[1,"absolute","bottom-0","left-1/2","w-0","h-1","bg-white","group-hover:w-full","group-hover:left-0","transition-all","duration-300"]],template:function(a,c){a&1&&(e(0,"section",0)(1,"div",1)(2,"h2",2),n(3," About Me "),t(),e(4,"div",3)(5,"div",4),o(6,"img",5),t(),e(7,"div",6)(8,"h3",7),n(9," Who Am I? "),t(),e(10,"p",8),n(11," I'm Ayoub Dammak, a passionate Software Engineer with hands-on experience in web and mobile application development. I specialize in crafting efficient, scalable, and user-friendly solutions that solve real-world problems. "),t(),e(12,"p",9),n(13," I enjoy working on projects that challenge me to think critically and apply my skills in innovative ways. I thrive in collaborative environments and am always eager to learn and grow. "),t(),e(14,"a",10),n(15," Download My Resume "),o(16,"i",11)(17,"span",12),t()()()()())},styles:[`
+
+  @keyframes _ngcontent-%COMP%_fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes _ngcontent-%COMP%_slideIn {
+    from {
+      opacity: 0;
+      transform: translateX(-30px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+  }
+
+  .animate-fade-in[_ngcontent-%COMP%] {
+    animation: _ngcontent-%COMP%_fadeIn 1s ease-out;
+  }
+
+  .animate-slide-in[_ngcontent-%COMP%] {
+    animation: _ngcontent-%COMP%_slideIn 1s ease-out;
+  }`]})};export{d as a};
---
+++ browser/chunk-UU3YSINQ.js
@@ -0,0 +1,15 @@
+import{b as m,g as l,h as e,i as t,j as i,k as n,o as s}from"./chunk-LXRMDS5F.js";var d=class a{constructor(r){this.titleService=r;this.titleService.setTitle("Ayoub Dammak - Home")}static \u0275fac=function(o){return new(o||a)(l(s))};static \u0275cmp=m({type:a,selectors:[["app-home"]],decls:18,vars:0,consts:[["id","home",1,"py-16","bg-gradient-to-b","from-gray-100","to-gray-200","h-screen"],[1,"container","mx-auto","grid","grid-cols-1","md:grid-cols-2","gap-8","items-center","h-full"],["aria-label","Introduction Section",1,"text-center","md:text-left","animate-fade-in"],[1,"text-5xl","font-bold","text-gray-600","mb-2"],[1,"text-blue-600"],[1,"text-3xl","text-gray-800","mb-4"],[1,"text-lg","text-gray-700","mb-6","max-w-xl","mx-auto","md:mx-0"],["href","#contact",1,"mt-6","inline-block","bg-blue-600","text-white","px-8","py-3","rounded-md","shadow-lg","hover:bg-blue-700","hover:shadow-2xl","transform","hover:scale-105","transition-transform","duration-300","relative","group"],[1,"uil","uil-message","ml-2"],[1,"absolute","bottom-0","left-1/2","w-0","h-1","bg-white","transition-all","duration-300","group-hover:w-full","group-hover:left-0"],["aria-label","Profile Image Section",1,"flex","justify-center","md:justify-end","animate-fade-in"],["src","https://avatars.githubusercontent.com/u/137606998?v=4","alt","Ayoub Dammak, Software Engineer",1,"w-100","h-100","md:w-100","md:h-100","rounded-full","border-4","border-gray-300","shadow-lg","transform","hover:scale-105","transition-transform","duration-300"]],template:function(o,c){o&1&&(e(0,"section",0)(1,"div",1)(2,"div",2)(3,"h1",3),n(4," Hi, I'm "),e(5,"span",4),n(6,"Ayoub Dammak"),t(),n(7," \u{1F44B}\u{1F3FB} "),t(),e(8,"h3",5),n(9,"Software Engineer"),t(),e(10,"p",6),n(11," High-level experience in web and mobile development, problem-solving, and producing quality work. "),t(),e(12,"a",7),n(13," Contact Me "),i(14,"i",8)(15,"span",9),t()(),e(16,"div",10),i(17,"img",11),t()()())},styles:[`
+
+  @keyframes _ngcontent-%COMP%_fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  .animate-fade-in[_ngcontent-%COMP%] {
+    animation: _ngcontent-%COMP%_fadeIn 1.2s ease-out;
+  }`]})};export{d as a};
