5dac01129af0811e9041d7ebd6e2e48fe39fb6ee
AyoubDammaq <ayoubdammak81@gmail.com>
2025-01-28 22:35:53 +0100

    update contact and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,13 @@ import { ContactComponent } from './components/contact/contact.component';
 import { ResumeComponent } from './components/resume/resume.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
   { path: 'about', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) },
   { path: 'resume', loadChildren: () => import('./components/resume/resume.module').then(m => m.ResumeModule) },
   { path: 'projects', loadChildren: () => import('./components/projects/projects.module').then(m => m.ProjectsModule) },
   { path: 'contact', loadChildren: () => import('./components/contact/contact.module').then(m => m.ContactModule) },
-  { path: 'footer', loadChildren: () => import('./components/footer/footer.module').then(m => m.FooterModule) },
-  { path: '**', component: HomeComponent, pathMatch: 'full'}
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
